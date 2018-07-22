6ef3b8612fc03dbfcff18be043787ab582bbd841
ananevans <ananevans@gmail.com>
2018-07-22 15:29:21 -0400

    For each call determine to which crate it belongs

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # external_calls
+
+
+To compiler the crate: cargo build.
+
+To see the run on a copy of TockOS:
+cd examples/elf2tbf
+cargo build
--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,6 @@ extern crate rustc_plugin;
 extern crate syntax;
 
 use rustc_plugin::Registry;
-use std::collections::HashMap;
 use rustc::hir::Expr;
 use rustc::hir::Expr_;
 use rustc::hir::Ty_;
@@ -23,6 +22,11 @@ use rustc::lint::LintPass;
 use rustc::lint::LintArray;
 use rustc::hir::QPath;
 use rustc::hir::print;
+use rustc::hir::def_id::LOCAL_CRATE;
+use rustc::hir::def_id::DefId;
+use rustc::hir::def::Def;
+use rustc::hir;
+use rustc::ty::TyCtxt;
 
 struct ExternalCalls;
 //{
@@ -39,37 +43,118 @@ impl LintPass for ExternalCalls{
 }
 
 impl<'a, 'tcx> LateLintPass<'a, 'tcx> for ExternalCalls {
-    fn check_expr(&mut self, _: &LateContext, expr: &Expr) {
-//    fn check_stmt(&mut self, _cx: &EarlyContext, stm: &Stmt) {
-//        if let StmtKind::Expr(ref expr) = stm.node {
-            match expr.node {
-                Expr_::ExprCall(ref fname,_) => {
-                    match fname.node {
-                        Expr_::ExprPath(ref path) => {
-                            print_path(path);
-                            println!();
-                            println!("Call {:?}", fname);
-                        }                        
-                        _ => {
-                            println!("Call {:?}", fname);
+    fn check_expr(&mut self, cx: &LateContext, expr: &Expr) {
+        
+        let t = cx.tables.expr_ty(&expr);
+        let mut ty_warned = false;
+        let mut fn_warned = false;
+        let mut op_warned = false;
+        let maybe_def_id = match expr.node {
+            Expr_::ExprCall(ref callee, _) => {
+                match callee.node {
+                    Expr_::ExprPath(ref qpath) => {
+                        let def = cx.tables.qpath_def(qpath, callee.hir_id);
+                        if let Def::Fn(def_id) = def {
+                            Some(def_id)
+                        } else {  // `Def::Local` if it was a closure, for which we
+                            None  // do not currently support must-use linting
                         }
-                    }
+                    },
+                    _ => None
                 }
-                Expr_::ExprMethodCall(ref ps,_,_) => {
-                    println!("MethodCall {:?}", ps);
-                }               
-                _ => {}
-            }
+            },
+            Expr_::ExprMethodCall(..) => {
+                let def = cx.tables.type_dependent_defs().get(expr.hir_id).cloned();
+                match def {
+                    Some(hir::def::Def::Method(def_id)) => { Some(def_id) }
+                    _ =>  { None }
+                }
+            },
+            _ => None
+        };
+
+        match expr.node {
+            Expr_::ExprCall(..) |
+            Expr_::ExprMethodCall(..) => {
+                println!("===================================================");
+                println!("Expr {:?}", expr);
+                println!("Type {:?}", expr);
+        
+                match maybe_def_id {
+                    Some (did) =>
+                        if (did.krate == LOCAL_CRATE) {
+                            println!("Local crate name {:?}",
+                                     cx.tcx.crate_name(did.krate));
+                        } else {
+                            println!("External crate name {:?}",
+                                     cx.tcx.crate_name(did.krate));
+                        }
                 
-//        }
+                    None => { println!("def id not found") }
+                }
+            }
+            _ => {}
+        }
+            
+        
+        // match expr.node {
+        //     Expr_::ExprCall(ref fname,_) => {
+        //         match fname.node {
+        //             Expr_::ExprPath(ref path) => {
+        //                 print_path(path);
+        //                 println!();
+        //                 println!("Call {:?}", fname);
+        //                 let def_id = get_def_id(path);
+        //                 match def_id {
+        //                     Some (did) => {
+        //                         println!("Crate id {:?}",
+        //                                  ltcx.tcx.crate_name(did.krate));
+        //                     }
+        //                     None => {
+        //                         println!("Crate is not found");
+        //                     }
+        //                 }
+        //             }                        
+        //             _ => {
+        //                 println!("Call {:?}", fname);
+        //             }
+        //         }
+        //     }
+        //     Expr_::ExprMethodCall(ref ps,_,_) => {
+        //         println!("MethodCall Type {:?}", ltcx.tables.node_id_to_type(expr.hir_id));
+        //     }               
+        //     _ => {}
+        // }
     }
 }
 
-fn print_path(path: &QPath) {
-     match path {
-         QPath::Resolved(_, path1) => {
+fn get_def_id(p: &QPath) -> Option<DefId> {
+     match p {
+         QPath::Resolved(_, path) => {
+             match (*path).def {
+                 rustc::hir::def::Def::Fn(defId) => { return Some(defId) }
+                 _ => {}
+             }
+             // let v = path.into_vec();
+             // for p1 in &v {
+             //     match p1.def {
+             //         rustc::hir::def::Def::Fn(defId) => { return Some(defId) }
+             //         _ => {}
+             //     }
+             // }
+             return None
+         }
+         QPath::TypeRelative(ty, ps) => {
+             return None
+         }
+     }
+}
+
+fn print_path(p: &QPath) {
+     match p {
+         QPath::Resolved(_, path) => {
              //print!("{:?}", path1);
-             print!("{:?}", print::to_string(print::NO_ANN, |s| s.print_path(path1, false)))
+             print!("{:?}", print::to_string(print::NO_ANN, |s| s.print_path(path, false)))
          }
          QPath::TypeRelative(ty, ps) => {
              match ty.node {
