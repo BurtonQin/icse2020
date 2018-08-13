5b6665a7ae167513871856aac643d507bf8968d4
Ana Nora Evans <ananevans@gmail.com>
2018-08-12 20:51:03 -0400

    added tests

--- examples/hidden_unsafe_tests/src/main.rs
+++ examples/hidden_unsafe_tests/src/main.rs
@@ -1,6 +1,17 @@
 #![feature(plugin)]
+#![feature(asm)]
 
 #![plugin(hidden_unsafe)]
+
+pub unsafe fn with_asm() {
+    asm!("nop");
+}
+
+pub fn with_asm_unsafe_block() {
+    unsafe {
+        asm!("nop");
+    }
+}
 
 fn safe_fn() -> () {
     println!("safe_fn");
@@ -105,6 +116,7 @@ fn unsafe_in_closure() {
 fn call_unsafe(s1: &UnsafeImpl) {
     unsafe{s1.unsafe_method_unsafe_trait();}
     unsafe{s1.unsafe_method_safe_trait();}
+    with_asm_unsafe_block();
     let mut v = Vec::new();
     v.push(1);
 }
@@ -120,6 +132,31 @@ fn two_calls(s1: &UnsafeImpl) {
     call_unsafe(s1);
 }
 
+
+extern {
+    // Our C function definitions!
+    pub fn strcpy(dest: *mut u8, src: *const u8) -> *mut u8;
+    pub fn puts(s: *const u8) -> i32;
+}
+
+fn call_C() {
+    let x = b"Hello, world!\0"; // our string to copy
+    let mut y = [0u8; 32]; // declare some space on the stack to copy the string into
+    unsafe {
+      // calling C code is definitely unsafe. it could be doing ANYTHING
+      strcpy(y.as_mut_ptr(), x.as_ptr()); // we need to call .as_ptr() to get a pointer for C to use
+      puts(y.as_ptr());
+    }
+}
+
+unsafe fn call_unsafe_1() {
+    unsafe_fn();
+}
+
+unsafe fn call_unsafe_2() {
+    let s1 = UnsafeImpl {};
+    s1.unsafe_method_unsafe_trait();
+}
 
 fn main() {
     println!("Hello, world!");
@@ -140,5 +177,9 @@ fn main() {
     call_unsafe_and_safe(&s1);
     two_calls(&s1);
     UnsafeImpl::no_self_safe();
+    unsafe{with_asm();}
+    call_C();
+    unsafe{ call_unsafe_1();
+            call_unsafe_2(); }
     //SafeImpl::nested_methods();
 }
