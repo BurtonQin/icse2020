fc16f754a89c30d2f1274e0120157e2ca652eb2c
Ana Nora Evans <ananevans@gmail.com>
2018-08-30 10:18:39 -0400

    improved github related scripts

--- github-downloads/compile.sh
+++ github-downloads/compile.sh
@@ -1,15 +1,37 @@
 CRT_DIR=`pwd`
 
 HOME_DIR=/tmp/unsafe_analysis/github-downloads
-PLUGIN_HOME=/home/ans5k/work/external_calls
+PLUGIN_HOME=$HOME/work/external_calls
 
 cd $HOME_DIR
+
+function compile_1 {
+	echo "Processing $1"
+	cd $1
+	cargo +nightly build
+	cd ../
+}
+
+function compile_2 {
+	echo "Processing $1/$2"
+        cd $1/$2
+        cargo +nightly build
+        cd ../../
+}
 
 
 export RUSTFLAGS="--extern hidden_unsafe=$PLUGIN_HOME/hidden_unsafe/target/debug/libhidden_unsafe.so -Z extra-plugins=hidden_unsafe --emit mir"
 
-cd xi-editor/rust
-cargo +nightly build
+compile_2 xi-editor rust
+
+#TODO add servo when the nighlty version is 2018-08-29
+
+compile_1 alacritty
+compile_1 ripgrep
+compile_1 xray
+compile_1 fd
+compile_1 leaf
+compile_1 Rocket
 
 cd $CRT_DIR
 
--- github-downloads/download.sh
+++ github-downloads/download.sh
@@ -3,6 +3,7 @@ function update {
 	then
         	cd $1
 	        git pull
+		cd ..
 	else
         	git clone $2
 	fi
@@ -18,5 +19,11 @@ cd $HOME_DIR
 
 update xi-editor https://github.com/google/xi-editor
 update servo https://github.com/servo/servo.git
+update alacritty https://github.com/jwilm/alacritty.git
+update ripgrep https://github.com/BurntSushi/ripgrep.git
+update xray https://github.com/atom/xray.git
+update fd https://github.com/sharkdp/fd.git
+update leaf https://github.com/autumnai/leaf.git
+update Rocket https://github.com/SergioBenitez/Rocket.git
 
 cd $CRT_DIR
