0fd5abbb3f8836d92939cdd33dac5471b7b8d486
Ana Nora Evans <ananevans@gmail.com>
2018-09-04 14:31:26 -0400

    update

--- select-crates/compile.sh
+++ select-crates/compile.sh
@@ -1,17 +1,20 @@
 #/bin/bash
 
 CRT_DIR=`pwd`
-PROJECT_HOME="/home/nora/work/external_calls"
+PROJECT_HOME="/home/ans5k/work/external_calls"
 
 CRATES_DIR=/tmp/unsafe_analysis/crates.io-downloads/
+
+rustup override set nightly-2018-08-29
+
+export RUSTFLAGS="--extern hidden_unsafe=$PROJECT_HOME/hidden_unsafe/target/debug/libhidden_unsafe.so -Z extra-plugins=hidden_unsafe --emit mir"
 
 cd $CRATES_DIR
 for d in $(ls $CRATES_DIR)
 do
 	echo "Compiling $d"
 	cd $d
-	export RUSTFLAGS="--extern hidden_unsafe=$PROJECT_HOME/hidden_unsafe/target/debug/libhidden_unsafe.so -Z extra-plugins=hidden_unsafe --emit mir"
-	cargo +nightly build
+	cargo build
 	cd ..
 done
 
