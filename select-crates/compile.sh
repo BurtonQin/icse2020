9b42f37837eed5aba19a373e05c7b888d064ccff
Ana Nora Evans <ananevans@gmail.com>
2018-09-04 05:49:12 -0400

    fixed download scripts

---
+++ select-crates/compile.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+CRT_DIR=`pwd`
+PROJECT_HOME="/home/nora/work/external_calls"
+
+CRATES_DIR=/tmp/unsafe_analysis/crates.io-downloads/
+
+for d in $(ls $CRATES_DIR)
+do
+	echo "Compiling $d"
+	cd $d
+	export RUSTFLAGS="--extern hidden_unsafe=$PROJECT_HOME/hidden_unsafe/target/debug/libhidden_unsafe.so -Z extra-plugins=hidden_unsafe --emit mir"
+	cargo +nightly build
+	cd ..
+done
+
+cd $CRT_DIR
+
---
+++ select-crates/crates_select_and_download.sh
@@ -0,0 +1,59 @@
+#/bin/bash
+
+## Collect information for each crate 
+
+OUTPUT_DIR=/tmp/unsafe_analysis/crates.io-downloads
+
+CRT_DIR=`pwd`
+
+if [ ! -f crates.io-fixed ] 
+then 
+	cd ../../
+	if [ -d crates.io-index ] 
+	then
+		cd crates.io-index
+		git pull
+		CRATES_IO_INDEX_HOME=`pwd`
+	else 
+		git clone https://github.com/rust-lang/crates.io-index.git
+		CRATES_IO_INDEX_HOME=`pwd`/crates.io-index
+	fi
+
+	echo "crates.io-index updated, home $CRATES_IO_INDEX_HOME"
+
+	cd $CRT_DIR
+
+	rm -f crates.io
+
+	for crate in $(find $CRATES_IO_INDEX_HOME -type f -printf '%f\n')
+	do
+		echo "Processing crate $crate"
+		curl https://crates.io/api/v1/crates/$crate >> crates.io
+		res=$?
+		if test "$res" != "0"
+		then
+	 		echo "Query failed for crate: $crate"
+		fi
+		echo "" >> crates.io
+	done
+	grep -v "\"detail\":\"Not Found\"" crates.io >> crates.io-fixed
+fi
+
+FILE=`pwd`/crates.io-fixed
+cargo run -- 10 $FILE > top-N-crates.io
+
+cat top-N-crates.io | sed 's/\"//g' > top-N-crates.io
+
+rm -rf $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR
+
+while read line
+do
+	crate=$(echo $line | cut -f 1)
+	echo "cloning crate $crate"
+	#cargo clone $crate
+done <top-N-crates.io
+
+cd $CRT_DIR
+
+
