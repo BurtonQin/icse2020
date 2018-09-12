2f0971ac91e036fd330d9f52da422784a15f667f
Ana Nora Evans <ananevans@gmail.com>
2018-09-12 15:07:20 -0400

    update

--- examples/tests/compile.sh
+++ examples/tests/compile.sh
@@ -2,7 +2,7 @@ PROJECT_HOME="$HOME/work/unsafe_study"
 
 export RUSTFLAGS="--extern unsafe_analysis=$PROJECT_HOME/unsafe-analysis/target/debug/libunsafe_analysis.so -Z extra-plugins=unsafe_analysis --emit mir"
 export RUST_BACKTRACE=1
-export RUST_LOG="unsafe_analysis=debug"
+export RUST_LOG="unsafe_analysis="
 
 cargo +nightly-2018-09-08  $1
 
--- unsafe-analysis/src/calls.rs
+++ unsafe-analysis/src/calls.rs
@@ -5,7 +5,6 @@ use rustc::mir::visit::Visitor;
 use rustc::mir::{BasicBlock, Location, Operand, Terminator, TerminatorKind};
 use rustc::ty;
 use rustc::ty::TyKind;
-use rustc::ty::subst::Subst;
 use fn_info;
 use util;
 
@@ -48,54 +47,38 @@ impl<'a, 'tcx> Visitor<'tcx> for CallsVisitor<'a, 'tcx> {
             args: _,
             destination: _,
             cleanup: _,
-        } = terminator.kind
-        {
+        } = terminator.kind {
             if let Operand::Constant(constant) = func {
-                //                println!("func {:?} kind {:?}", func, constant.literal.ty.sty);
-                //if let TyKind::FnDef(callee_def_id, substs) = constant.literal.ty.sty {
-                // TODO here maybe fix the bug
                 if let TyKind::FnDef(callee_def_id, substs) = constant.literal.ty.sty {
-                    let param_env = self
-                        .cx
-                        .tcx
-                        .param_env(self.cx.tcx.hir.local_def_id(self.fn_info.decl_id()));
+                      if callee_def_id.is_local() {
+                          let param_env = self.cx.tcx.param_env(self.cx.tcx.hir.local_def_id(self.fn_info.decl_id()));
+                          if let Some(instance) = ty::Instance::resolve(self.cx.tcx, param_env, callee_def_id, substs) {
+                                match instance.def {
+                                    ty::InstanceDef::Item(def_id)
+                                    | ty::InstanceDef::Intrinsic(def_id)
+                                    | ty::InstanceDef::Virtual(def_id, _) => {
+                                        let node_id = self.cx.tcx.hir.def_index_to_node_id(def_id.index);
+                                        self.fn_info.push_local_call(node_id);
 
-                    if let Some(instance) =
-                        ty::Instance::resolve(self.cx.tcx, param_env, callee_def_id, substs)
-                    {
-                        match instance.def {
-                            ty::InstanceDef::Item(def_id)
-                            | ty::InstanceDef::Intrinsic(def_id)
-                            | ty::InstanceDef::Virtual(def_id, _) => {
-                                if let Some(decl_node_id) = self.cx.tcx.hir.as_local_node_id(def_id)
-                                {
-                                    //println!("func {:?} pushed local", func);
-                                    self.fn_info.push_local_call(decl_node_id);
-                                } else {
-                                    //                                    println!("func {:?} pushed external", func);
-                                    let safety =
-                                        match self.cx.tcx.fn_sig(callee_def_id).unsafety() {
-                                            hir::Unsafety::Unsafe => {fn_info::Safety::Unsafe}
-                                            hir::Unsafety::Normal => {fn_info::Safety::Normal}
-                                        };
-                                    debug!("tcx.type_of(self.did).subst(tcx, subst)={:?}"
-                                           , self.cx.tcx.type_of(callee_def_id).subst(self.cx.tcx, substs));
-                                    self.fn_info.push_external_call(self.cx, def_id,safety);
+                                    }
+                                    _ => error!("ty::InstanceDef:: NOT handled {:?}", instance.def),
                                 }
-                            }
-                            _ => error!("ty::InstanceDef:: NOT handled {:?}", instance.def),
-                        }
+                          } else {
+                              // Generics
+                              let node_id = self.cx.tcx.hir.def_index_to_node_id(callee_def_id.index);
+                              self.fn_info.push_local_call(node_id);
+                          }
                     } else {
-                        //                        println!("func {:?} pushed external", func);
                         let safety =
                             match self.cx.tcx.fn_sig(callee_def_id).unsafety() {
                                 hir::Unsafety::Unsafe => {fn_info::Safety::Unsafe}
                                 hir::Unsafety::Normal => {fn_info::Safety::Normal}
                             };
-                        debug!("tcx.type_of(self.did).subst(tcx, subst)={:?}"
-                               , self.cx.tcx.type_of(callee_def_id).subst(self.cx.tcx, substs));
+                        info!("External call {:?}", func);
+
                         self.fn_info.push_external_call(self.cx, callee_def_id,safety);
                     }
+
                 } else {
                     error!("TypeVariants NOT handled {:?}", constant.literal.ty.sty);
                 }
