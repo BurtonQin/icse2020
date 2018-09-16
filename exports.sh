47e272c14f0198d0d1448af89010ec75a494eb76
Ana Nora Evans <ananevans@gmail.com>
2018-09-15 23:36:30 -0400

    script to run geigerwq

--- exports.sh
+++ exports.sh
@@ -1,9 +1,12 @@
 export PROJECT_HOME=$HOME/work/unsafe_study
 
 export PROJECT_OUT=${HOME}/unsafe_analysis
+export CRATES_IO_INDEX_HOME=${HOME}/work/crates.io-index
 
 export CRATES_DIR=${PROJECT_OUT}/crates.io-downloads
 export UNSAFE_ANALYSIS_DIR=${PROJECT_OUT}/analysis_data
+export EXCLUDED_CRATES=${PROJECT_OUT}/excluded-crates
+export GEIGER_CRATES=${PROJECT_OUT}/geiger-crates
 
 export NIGHTLY=nightly-2018-09-08
 
