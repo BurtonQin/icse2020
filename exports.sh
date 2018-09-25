4440b3b419ed016c188aed6326a5a76894a59f8c
Ana Nora Evans <ananevans@gmail.com>
2018-09-25 16:50:14 -0400

    clean up

--- exports.sh
+++ exports.sh
@@ -2,16 +2,18 @@ source ~/.bash_profile
 
 export PROJECT_HOME=$HOME/work/unsafe_study
 
-export OSHA_HOME=$HOME/software/cargo-osha
+export OSHA_HOME=$HOME/work/cargo-osha
 
 export PROJECT_OUT=${HOME}/unsafe_analysis
 export CRATES_IO_INDEX_HOME=${HOME}/work/crates.io-index
 
 export CRATES_DIR=${PROJECT_OUT}/crates.io-downloads
-export UNSAFE_ANALYSIS_DIR=${PROJECT_OUT}/analysis_data
+export UNSAFE_ANALYSIS_DIR=${PROJECT_OUT}/analysis-data
 export EXCLUDED_CRATES=${PROJECT_OUT}/excluded-crates
-export GEIGER_CRATES=${PROJECT_OUT}/geiger-crates
+export SYNTACTIC_ONLY_CRATES=${PROJECT_OUT}/syntactic-only
 export GITHUB_APPS=${PROJECT_OUT}/github-downloads
+
+export PAPER_RESULTS_DIR=$PROJECT_HOME/paper/
 
 export NIGHTLY=nightly-2018-09-11
 
--- gnuplot-scripts/run_all.sh
+++ gnuplot-scripts/run_all.sh
@@ -1,9 +1,15 @@
 #/bin/bash
 
-./rq01/run.sh
-./rq02/run.sh
-./rq03/run.sh
-./rq04/run.sh
-./rq05/run.sh
-./rq06/run.sh
-./rq09/run.sh
+source ../exports.sh
+
+mkdir -p $PAPER_RESULTS_DIR
+
+./geiger.R
+
+#./rq01/run.sh
+#./rq02/run.sh
+#./rq03/run.sh
+#./rq04/run.sh
+#./rq05/run.sh
+#./rq06/run.sh
+#./rq09/run.sh
