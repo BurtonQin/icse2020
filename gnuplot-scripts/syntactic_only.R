15cddc4cfc15427ed04998723b3bfa978c6f69c6
Ana Nora Evans <ananevans@gmail.com>
2018-09-26 06:25:07 -0400

    fixed unsafe functions graph

--- gnuplot-scripts/syntactic_only.R
+++ gnuplot-scripts/syntactic_only.R
@@ -2,9 +2,6 @@
 library(ggplot2)
 library(Hmisc)
 
-table_filename <- "~/work/unsafe_study/paper/syntactic_func_table.txt"
-cdf_filename <- "~/work/unsafe_study/paper/syntactic_func_cdf.eps"
-
 res <- read.table( file="/home/nora/unsafe_analysis/analysis-data/syntactic_only_results.txt"
                    , header=FALSE
                    , sep=','
@@ -12,14 +9,37 @@ res <- read.table( file="/home/nora/unsafe_analysis/analysis-data/syntactic_only
                    , col.names=c("functions", "traits", "blocks", "name"))
 
 
+table_filename <- "~/work/unsafe_study/paper/syntactic_func_table.txt"
+cdf_filename <- "~/work/unsafe_study/paper/syntactic_func_cdf.eps"
 ft <- describe(res$functions)
 latex(ft,file="~/work/unsafe_study/paper/syntactic_func_table.txt")
-
+top5_x <- res$functions[order(res$functions,decreasing = TRUE )[1:5]]
+min_y <- nrow(subset(res,functions==0)) / nrow(res)
+first_y <- ceiling(min_y*100)/100 + 0.01
 ggplot(res, aes(functions)) + 
   stat_ecdf(geom = "step") +
+  scale_x_continuous(breaks=c(seq(0,top5_x[4],100),top5_x[1:4])) +
+  scale_y_continuous(limits=c(min_y,1),breaks = c(min_y, seq(first_y,1,0.01))) +
+  theme(axis.text.x=element_text(angle=90, hjust=1))+
   xlab("Cumulative Distribution") + 
   ylab("Number of Crates") +
-  labs(title="Cumulative Distribution of Unsafe Functions") +
-
+  labs(title="Cumulative Distribution of Unsafe Functions") 
 ggsave(cdf_filename, plot = last_plot(), device = "eps")
 
+
+table_filename <- "~/work/unsafe_study/paper/syntactic_block_table.txt"
+cdf_filename <- "~/work/unsafe_study/paper/syntactic_block_cdf.eps"
+ft <- describe(res$blocks)
+latex(ft,file="~/work/unsafe_study/paper/syntactic_block_table.txt")
+top5_x <- res$blocks[order(res$blocks,decreasing = TRUE )[1:5]]
+min_y <- nrow(subset(res,blocks==0)) / nrow(res)
+first_y <- ceiling(min_y*100)/100 + 0.01
+ggplot(res, aes(blocks)) + 
+  stat_ecdf(geom = "step") +
+  scale_x_continuous(breaks=c(seq(0,top5_x[4],100),top5_x[1:4])) +
+  scale_y_continuous(limits=c(min_y,1),breaks = c(min_y, seq(first_y,1,0.01))) +
+  theme(axis.text.x=element_text(angle=90, hjust=1))+
+  xlab("Cumulative Distribution") + 
+  ylab("Number of Crates") +
+  labs(title="Cumulative Distribution of Unsafe Blocks") 
+ggsave(cdf_filename, plot = last_plot(), device = "eps")
