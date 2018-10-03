d75a6360473928e991b964b75bf6dc8f305c35ec
Ana Nora Evans <ananevans@gmail.com>
2018-10-03 10:23:22 -0400

    cut off tail from graphs of rq01 and rq02

--- gnuplot-scripts/rq02.R
+++ gnuplot-scripts/rq02.R
@@ -1,6 +1,9 @@
 #!/usr/bin/env Rscript
 library(ggplot2)
+library(reshape2)
+library(plyr)
 library(Hmisc)
+library(scales)
 
 res <- read.table( file="~/unsafe_analysis/analysis-data/research-questions/rq02"
                    , header=FALSE
@@ -11,9 +14,10 @@ res <- read.table( file="~/unsafe_analysis/analysis-data/research-questions/rq02
 table_filename <- "~/work/unsafe_study/paper/rq02_table.txt"
 cdf_filename <- "~/work/unsafe_study/paper/rq02_cdf.eps"
 base_filename <- "~/work/unsafe_study/paper/rq02_"
+outliers_filename <- "~/work/unsafe_study/paper/rq02_outliers.txt" 
 
 #table
-summary <- quantile(res$functions, c(.90,.95))
+summary <- quantile(res$functions, c(.90,.95,.99))
 fn <- paste0(base_filename,"n",".txt")
 write(nrow(res),file=fn)
 p90 <- paste0(base_filename,"90",".txt")
@@ -21,28 +25,44 @@ write(summary[1],file=p90)
 p95 <- paste0(base_filename,"95",".txt")
 write(summary[2],file=p95)
 
-top5_x <- res$functions[order(res$functions,decreasing = TRUE )[1:5]]
+
+#graph 
+ggdata <- data.frame(res$functions)
+ggdata <- melt(ggdata)
+ggdata <- ddply(ggdata, .(variable), transform, ecdf=ecdf(value)(value))
+
 min_y <- length( res$functions[res$functions==0] ) / length(res$functions)
 first_y <- ceiling(min_y*10)/10
-functions.q <- mean(res$functions)
-ggplot(NULL, aes(x=res$functions)) +
-  geom_step(stat="ecdf") +
-  geom_vline(aes(xintercept=functions.q), linetype="dashed") +
-  xlab("Unsafe Functions") + 
+
+x_max <- summary[3]
+outliers <- subset(res,functions>summary[3])
+
+ggplot(ggdata, aes(x=value, y=ecdf)) +
+  geom_point()+
+  xlab("Unsafe Functions") +
   ylab("Percent of Crates") +
   labs(title="Cumulative Distribution of Unsafe Functions") +
   scale_x_continuous(
-    breaks=c(seq(0,top5_x[1],500))
-    , limits = c(0,top5_x[1])
+    breaks=c(seq(0,x_max,10),x_max)
+    , limits = c(0,x_max+1)
     , labels = comma
   ) +
   theme(axis.text.x=element_text(angle=90, hjust=1)) +
   scale_y_continuous(
     limits = c(min_y-0.01,1)
-    , breaks = c(min_y, seq(first_y,1,0.01))
+    , breaks = c(min_y, seq(first_y,1,0.05))
     ,labels = percent
   )
 ggsave(cdf_filename, plot = last_plot(), device = "eps")
+
+#outliers
+write(min(outliers$functions), file=outliers_filename)
+write(" and ", file=outliers_filename,append=TRUE)
+write(max(outliers$functions), file=outliers_filename,append=TRUE)
+write(" (", file=outliers_filename,append=TRUE)
+write(nrow(outliers), file=outliers_filename,append=TRUE)
+write(" values).", file=outliers_filename,append=TRUE)
+
 
 #save number of crates with at least one unsafe
 options(digits = 4)
